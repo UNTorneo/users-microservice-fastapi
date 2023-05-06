ff947e4951fb736e55f146ae6a449da1f0bdaef2
Valentina Colmenares <vcolmenares@unal.edu.co>
2023-05-06 13:10:30 -0500

    Readme con base de datos

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+Comandos para ejecutar la base datos
+docker build -t fastapi-db .
+docker run -d -t -i -p 5432:5432 --name fastapi-db fastapi-db
+
+Dockerfile de la base de datos
+FROM postgres:15.2
+ENV POSTGRES_PASSWORD=123
+ENV POSTGRES_USER=fastapi
+ENV POSTGRES_DB=fastapi_db
+EXPOSE 5432
+
+Ejecutar microservicio en docker
 docker build -t users-microservice-fastapi .
 docker run -p 8080:8080 -e dbHost=172.17.0.2 -e dbPort=5432 -e dbUser=fastapi -e dbPassword=123 -e dbName=fastapi_db -d users-microservice-fastapi
 
@@ -6,7 +18,8 @@ docker container ls -a -> Muestra lista contenedores -> Extraes el CONTAINER_ID 
 docker container start CONTAINER_ID
 docker container stop CONTAINER_ID
 
-842e49b74d41301405f97a9206c68fd3715408c87a5ab5eab3f653d1443017fb
-
+Para ejecutar en el local
 uvicorn app.main:app --reload
+
+Swagger
 http://127.0.0.1:8080/docs
--- app/router/urls.py
+++ app/router/urls.py
@@ -27,7 +27,7 @@ async def loginForAccessToken(user: schemas.Login, db: Session = Depends(getDb))
     if not dbUser:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Email o contraseña incorrecta",
             headers={"WWW-Authenticate": "Bearer"},
         )
     accessTokenExpires = timedelta(minutes=accessTokenExpireMinutes)
@@ -40,33 +40,35 @@ async def loginForAccessToken(user: schemas.Login, db: Session = Depends(getDb))
 def createUser(user: schemas.UserCreate, db: Session = Depends(getDb)):
     dbUser = crud.getUserByEmail(db, email=user.email)
     if dbUser:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="Email ya registrado")
     return crud.createUser(db=db, user=user)
 
 @router.get("/users/", response_model=list[schemas.User])
 def readUsers(skip: int = 0, limit: int = 100, db: Session = Depends(getDb)):
     users = crud.getUsers(db, skip=skip, limit=limit)
+    if users is None:
+        raise HTTPException(status_code=404, detail="No hay usuarios registrados")
     return users
 
 @router.get("/users/{userId}", response_model=schemas.User)
 def readUser(userId: int, db: Session = Depends(getDb)):
     dbUser = crud.getUser(db, userId=userId)
     if dbUser is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return dbUser
 
 @router.put("/users/{usersId}", response_model=ResponseModel)
 def updateUser(user:schemas.UserUpdate, db: Session = Depends(getDb)):  
     dbUser = db.query(models.User).filter(models.User.id == user.id).first()
     if dbUser is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return crud.putUser(db=db, user=user)
 
 @router.delete("/users/{userId}", response_model=ResponseModel)
 def deleteUser(userId: int, db: Session = Depends(getDb)):
     dbUser = crud.getUser(db, userId=userId)
     if dbUser is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return crud.removeUser(db=db, id=userId)
 
 
@@ -74,33 +76,35 @@ def deleteUser(userId: int, db: Session = Depends(getDb)):
 def createCountry(country: schemas.CountryCreate, db: Session = Depends(getDb)):
     dbCountry = crud.getCountryByName(db, name=country.name)
     if dbCountry:
-        raise HTTPException(status_code=400, detail="Country already registered")
+        raise HTTPException(status_code=400, detail="País ya registrado")
     return crud.createCountry(db=db, country=country)
 
 @router.get("/countries/", response_model=list[schemas.Country])
 def readCountries(skip: int = 0, limit: int = 100, db: Session = Depends(getDb)):
     countries = crud.getCountries(db, skip=skip, limit=limit)
+    if countries is None:
+        raise HTTPException(status_code=404, detail="No hay países registrados")
     return countries
 
 @router.get("/countries/{countryId}", response_model=schemas.Country)
 def readCountry(countryId: int, db: Session = Depends(getDb)):
     dbCountry = crud.getCountry(db, countryId=countryId)
     if dbCountry is None:
-        raise HTTPException(status_code=404, detail="Country not found")
+        raise HTTPException(status_code=404, detail="País no encontrado")
     return dbCountry
 
 @router.put("/countries/{countryId}", response_model=ResponseModel)
 def updateCountry(country: schemas.CountryUpdate, db: Session = Depends(getDb)):
     dbCountry = db.query(models.Country).filter(models.Country.id == country.id).first()
     if dbCountry is None:
-        raise HTTPException(status_code=404, detail="Country not found")
+        raise HTTPException(status_code=404, detail="País no encontrado")
     return crud.putCountry(db=db, country=country)
 
 @router.delete("/countries/{countryId}", response_model=ResponseModel)
 def deleteCountry(countryId: int, db: Session = Depends(getDb)):
     dbCountry = crud.getCountry(db, countryId=countryId)
     if dbCountry is None:
-        raise HTTPException(status_code=404, detail="Country not found")
+        raise HTTPException(status_code=404, detail="País no encontrado")
     return crud.removeCountry(db=db, id=countryId)
 
 
@@ -108,31 +112,33 @@ def deleteCountry(countryId: int, db: Session = Depends(getDb)):
 def createCity(city: schemas.CityCreate, db: Session = Depends(getDb)):
     dbCity = crud.getCityByName(db, name=city.name)
     if dbCity:
-        raise HTTPException(status_code=400, detail="City already registered")
+        raise HTTPException(status_code=400, detail="Ciudad ya registrada")
     return crud.createCity(db=db, city=city)
 
 @router.get("/cities/", response_model=list[schemas.City])
 def readCities(skip: int = 0, limit: int = 100, db: Session = Depends(getDb)):
     cities = crud.getCities(db, skip=skip, limit=limit)
+    if cities is None:
+        raise HTTPException(status_code=404, detail="No hay ciudades registradas")
     return cities
 
 @router.get("/cities/{cityId}", response_model=schemas.City)
 def readCity(cityId: int, db: Session = Depends(getDb)):
     dbCity = crud.getCity(db, cityId=cityId)
     if dbCity is None:
-        raise HTTPException(status_code=404, detail="City not found")
+        raise HTTPException(status_code=404, detail="Ciudad no encontrada")
     return dbCity
 
 @router.put("/cities/{cityId}", response_model=ResponseModel)
 def updateCity(city: schemas.CityUpdate, db: Session = Depends(getDb)):
     dbCity = crud.getCity(db, cityId=city.id)
     if dbCity is None:
-        raise HTTPException(status_code=404, detail="City not found")
+        raise HTTPException(status_code=404, detail="Ciudad no encontrada")
     return crud.putCity(db=db, city=city)
 
 @router.delete("/cities/{cityId}", response_model=ResponseModel)
 def deleteCity(cityId: int, db: Session = Depends(getDb)):
     dbCity = crud.getCity(db, cityId=cityId)
     if dbCity is None:
-        raise HTTPException(status_code=404, detail="City not found")
+        raise HTTPException(status_code=404, detail="Ciudad no encontrada")
     return crud.removeCity(db=db, id=cityId)
